ae476cd5eb981a7efcb1b502719e8415906491ed
MUHAMMAD HUSSAIN SHAMIM <142653978+hussainshamim16@users.noreply.github.com>
2024-05-26 07:39:06 +0500

    Delete function

--- script.js
+++ script.js
@@ -7,15 +7,25 @@ function AddTask() {
     var inputResult = document.getElementById("todoInput");
     var ul = document.getElementById("listUl");
     var li = document.createElement("li")
-  
+
     li.innerHTML = inputResult.value;
     ul.appendChild(li);
 
     var edit = document.createElement("button");
     edit.innerHTML = "EDIT";
-edit.setAttribute("onClick", "edit()")
+    edit.setAttribute("onClick", "edit(this)")
 
+    var del = document.createElement("button");
+    del.innerHTML = "DELETE";
+    del.setAttribute("onClick", "del(this)")
+
+    li.appendChild(edit);
+    li.appendChild(del);
 }
-// function edit() {
-//     console.log("ho gaya")
-// }
+
+function edit(ed) {
+    console.log("ho gaya");
+}
+function del(el) {
+   el.parentNode.remove();
+}
