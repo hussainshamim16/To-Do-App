452b94cf72766cf2d2c35c5f02120e95456cda48
MUHAMMAD HUSSAIN SHAMIM <142653978+hussainshamim16@users.noreply.github.com>
2024-05-29 22:44:45 +0500

    edit button

--- script.js
+++ script.js
@@ -4,10 +4,11 @@
 
 
 function AddTask() {
-   
     var inputResult = document.getElementById("todoInput");
     var ul = document.getElementById("listUl");
-    var li = document.createElement("li")
+    var li = document.createElement("li");
+    var div = document.createElement("div");
+    
     if(inputResult.value < 3){
         alert("Enter A Correct Task");
     return;
@@ -15,20 +16,32 @@ function AddTask() {
     li.innerHTML = inputResult.value;
     ul.appendChild(li);
 
+    li.appendChild(div);
+
+    var edit = document.createElement("button");
+    edit.innerHTML = "EDIT";
+    edit.setAttribute("onClick", "edit(this)")
+
+    edit.style.marginRight="20px"
+    div.appendChild(edit)
 
     var del = document.createElement("button");
     del.innerHTML = "DELETE";
     del.setAttribute("onClick", "del(this)")
 
+    div.appendChild(del);
+//    div.style.border =  "1px solid red";
+   div.style.width =  "20%";
+
+
     
-    li.appendChild(del);
 
     inputResult.value = "";
 }
 
 function del(del){
     
-    del.parentNode.remove()
+    del.parentNode.parentNode.remove()
 }
 var ten = 10;
 var nine = 9;
@@ -48,6 +61,17 @@ function hide(ele){
     }
     ele.children
 console.log(ele)
+}
+
+
+function edit(elem){
+    // var inputResult = document.getElementById("todoInput");
+    // var ul = document.getElementById("listUl");
+    // var li = document.createElement("li");
+    var set = prompt("Edit Value", elem.parentNode.parentNode.firstChild.innerHTML);
+    // elem.parentNode.parentNode.firstChild.innerHTML = set
+    // elem.innerHTML = set
+    console.log(elem.parentNode.parentNode.firstChild.innerHTML = set)
 }
 
 function color(cal){
